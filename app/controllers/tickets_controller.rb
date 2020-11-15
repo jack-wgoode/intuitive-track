f7976f4118321764b36777abd34081f03e7ad365
jack-wgoode <jack.w.goode@icloud.com>
2020-11-15 16:53:58 -0500

    edited tickets control added CRUD

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,2 +1,62 @@
 class TicketsController < ApplicationController
+    before_action :set_ticket, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @tickets = Ticket.all 
+    end
+
+    def new
+        @ticket = Ticket.new(end_user_id: params[:end_user_id])
+    end
+
+    def create
+        @ticket = current_user.tickets.build(ticket_params)
+        if @ticket.save
+            redirect_to ticket_path(@ticket)
+        else 
+            render :new
+        end
+    end
+
+    def show       
+       # implicit 
+    end
+
+    def edit
+        if @ticket.technician.id == current_user.id
+            render :edit
+        else
+            redirect_to ticket_path(@ticket)
+        end
+    end
+
+    def update
+        if @ticket.update(ticket_params)
+            redirect_to ticket_path(@ticket)
+        else 
+            render :edit
+        end
+    end
+
+    def destroy
+        if @ticket.technician.id == current_user.id
+            @ticket.destroy
+            redirect_to tickets_path
+        else
+            redirect_to ticket_path(@ticket)
+        end
+    end
+
+    private
+
+    def set_ticket
+        @ticket = Ticket.find(params[:id])
+    end
+
+    def ticket_params
+        params.require(:ticket).permit(:ticket_number, :asset, :issue, :priority, :end_user_id)
+    end
+
+
+
 end
